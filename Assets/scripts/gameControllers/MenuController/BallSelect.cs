9384d4ec1f22ccc42793e1ffa50d70a0a0b5db4e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-12 23:19:44 +0100

    Add SelectBall function

---
+++ Assets/scripts/gameControllers/MenuController/BallSelect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallSelect : MonoBehaviour {
+
+	private List<Button> buttons = new List<Button>();
+
+	private void Awake () {
+		GetButtonsAndAddListeners ();
+	}
+
+	public void SelectBall () {
+		int index = int.Parse (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+		print (index);
+	}
+
+	private void GetButtonsAndAddListeners () {
+		GameObject[] btns = GameObject.FindGameObjectsWithTag ("menuBall");
+
+		foreach (GameObject btn in btns) {
+			buttons.Add(btn.GetComponent<Button>());
+			btn.GetComponent<Button>().onClick.AddListener (() => SelectBall ());
+		}
+	}
+}
