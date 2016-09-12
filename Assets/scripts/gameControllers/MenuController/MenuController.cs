86c633bf5f0128eb308d39d43374c06a35859905
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-12 23:10:31 +0100

    Init repo and add MenuController

---
+++ Assets/scripts/gameControllers/MenuController/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	private Animator menuAnim, ballAnim;
+
+	private void Awake () {
+		menuAnim = GameObject.FindGameObjectWithTag ("menuAnim").GetComponent<Animator> ();
+		ballAnim = GameObject.FindGameObjectWithTag ("ballAnim").GetComponent<Animator> ();
+	}
+
+	public void PlayGame () {
+		SceneManager.LoadScene ("main", LoadSceneMode.Single);
+	}
+
+	public void SelectBall () {
+		menuAnim.Play ("slideOut");
+		ballAnim.Play ("slideIn");
+	}
+
+	public void BackToMenu () {
+		menuAnim.Play ("slideIn");
+		ballAnim.Play ("slideOut");
+	}
+}
